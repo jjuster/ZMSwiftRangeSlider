0c17ac2466c2be0aac0ad28ccd354a37ff5e575d
Josh Juster <juster@gmail.com>
2016-09-19 16:19:43 -0400

    support for single slider (max = -1)

--- Source/TrackLayer.swift
+++ Source/TrackLayer.swift
@@ -16,9 +16,14 @@ class TrackLayer: CALayer {
         CGContextAddPath(ctx, path.CGPath)
         CGContextFillPath(ctx)
 
-        CGContextSetFillColorWithColor(ctx, slider.trackHighlightTintColor.CGColor)
+        let color = slider.maxValue == -1 ? slider.trackTintColor : slider.trackHighlightTintColor
+        
+        CGContextSetFillColorWithColor(ctx, color.CGColor)
         let minValueOffsetX = slider.position(slider.minValue)
-        let maxValueOffsetX = slider.position(slider.maxValue)
+        
+        let maxValue = slider.maxValue == -1 ? slider.maxRange() : slider.maxValue
+        
+        let maxValueOffsetX = slider.position(maxValue)
         let rect = CGRect(x: minValueOffsetX, y: 0.0, width: maxValueOffsetX - minValueOffsetX, height: bounds.height)
         CGContextFillRect(ctx, rect)
     }
