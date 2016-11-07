15dfeec0553274ef6958bfbc9375f963c689ed0c
Josh Juster <juster@gmail.com>
2016-11-07 01:28:39 -0500

    swift3

--- Source/TrackLayer.swift
+++ Source/TrackLayer.swift
@@ -4,27 +4,27 @@ class TrackLayer: CALayer {
 
     weak var rangeSlider: RangeSlider?
 
-    override func drawInContext(ctx: CGContext) {
+    override func draw(in ctx: CGContext) {
         guard let slider = rangeSlider else {
             return
         }
 
         let path = UIBezierPath(roundedRect: bounds, cornerRadius: bounds.height * 0.5)
-        CGContextAddPath(ctx, path.CGPath)
+        ctx.addPath(path.cgPath)
 
-        CGContextSetFillColorWithColor(ctx, slider.trackTintColor.CGColor)
-        CGContextAddPath(ctx, path.CGPath)
-        CGContextFillPath(ctx)
+        ctx.setFillColor(slider.trackTintColor.cgColor)
+        ctx.addPath(path.cgPath)
+        ctx.fillPath()
 
         let color = slider.maxValue == -1 ? slider.trackTintColor : slider.trackHighlightTintColor
         
-        CGContextSetFillColorWithColor(ctx, color.CGColor)
+        ctx.setFillColor(color.cgColor)
         let minValueOffsetX = slider.position(slider.minValue)
         
         let maxValue = slider.maxValue == -1 ? slider.maxRange() : slider.maxValue
         
         let maxValueOffsetX = slider.position(maxValue)
         let rect = CGRect(x: minValueOffsetX, y: 0.0, width: maxValueOffsetX - minValueOffsetX, height: bounds.height)
-        CGContextFillRect(ctx, rect)
+        ctx.fill(rect)
     }
 }
