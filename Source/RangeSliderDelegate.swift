50c075dc9e5ddf912417265964a6b241129ffddd
Josh Juster <juster@gmail.com>
2016-11-07 01:27:52 -0500

    swift3

--- Source/RangeSliderDelegate.swift
+++ Source/RangeSliderDelegate.swift
@@ -2,9 +2,9 @@ import UIKit
 
 public protocol RangeSliderDelegate {
 
-    func rangeSliderValueChanged(sender: RangeSlider, minValue: Int, maxValue: Int)
+    func rangeSliderValueChanged(_ sender: RangeSlider, minValue: Int, maxValue: Int)
 
-    func rangeSliderMinValueDisplayText(sender: RangeSlider, minValue: Int) -> String?
+    func rangeSliderMinValueDisplayText(_ sender: RangeSlider, minValue: Int) -> String?
 
-    func rangeSliderMaxValueDisplayText(sender: RangeSlider, maxValue: Int) -> String?
+    func rangeSliderMaxValueDisplayText(_ sender: RangeSlider, maxValue: Int) -> String?
 }
